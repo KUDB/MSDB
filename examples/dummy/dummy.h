ab8b0019ac2ff33b8950205b3e8e23aecb68e20b
HyubjinLee <hyubjin487@gmail.com>
2021-09-08 19:45:29 +0900

    Add dummy star1024x1024

--- examples/dummy/dummy.h
+++ examples/dummy/dummy.h
@@ -1,7 +1,29 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <array/arrayDesc.h>
+
 #ifndef _MSDB_EXAMPLE_DUMMY_H_
 #define _MSDB_EXAMPLE_DUMMY_H_
 
 #define DUMMY_PATH(path) "..\\dummy\\"#path
+
+
+namespace msdb
+{
+namespace star1024x1024
+{
+	core::pDimensionDescs dimensionDescBuilder(
+		std::vector<std::string> dimNames, core::dimension& dims,
+		core::dimension& chunkDims, core::dimension& blockDims
+	);
+	core::pAttributeDescs attributeDescBuilder(
+		std::vector<std::string> attrNames,
+		std::vector<core::eleType> attrTypes
+	);
+
+	core::pArrayDesc getDummyArrayDesc_SIMPLE_2D();
+}		// star1024x1024
+}		// msdb
 
 #endif	// _MSDB_EXAMPLE_DUMMY_H_
