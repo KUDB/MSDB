e5670ce4e87a6571f336bcb1e90c30c8cb826bd9
RonyK <sangron@naver.com>
2022-08-28 06:24:51 +0900

    Change opEncodeAction->opIOAction

---
+++ msdb/include/query/opIOAction.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#ifndef _MSDB_IOACTION_H_
+#define _MSDB_IOACTION_H_
+
+#include <pch.h>
+#include <array/arrayId.h>
+#include <array/attributeId.h>
+#include <array/chunk.h>
+#include <query/query.h>
+#include <array/array.h>
+
+namespace msdb
+{
+namespace core
+{
+class opIOAction;
+using pEncodeAction = std::shared_ptr<opIOAction>;
+
+class opIOAction : public std::enable_shared_from_this<opIOAction>
+{
+public:
+	opIOAction();
+	virtual ~opIOAction();
+
+	virtual const char* name() = 0;
+
+public:
+	void encodeChunk(arrayId arrId, attributeId attrId, pChunk outChunk, pQuery qry, const size_t parentThreadId);
+	size_t getSerializedChunkSize(attributeId attrId, pArray arr);
+};
+}
+}
+#endif	// _MSDB_IOACTION_H_
---
+++ msdb/src/query/opIOAction.cpp
@@ -0,0 +1,46 @@
+﻿#include <pch.h>
+#include <query/opIOAction.h>
+#include <util/threadUtil.h>
+#include <system/storageMgr.h>
+
+namespace msdb
+{
+namespace core
+{
+opIOAction::opIOAction()
+{
+}
+opIOAction::~opIOAction()
+{
+}
+
+void opIOAction::encodeChunk(arrayId arrId, attributeId attrId, pChunk outChunk, pQuery qry, const size_t parentThreadId)
+{
+	auto threadId = getThreadId() + 1;
+	//========================================//
+	qry->getTimer()->nextJob(threadId, this->name() + std::string("::Thread"), workType::IO, std::string("chunk::") + std::to_string(outChunk->getId()));
+	//----------------------------------------//
+
+	storageMgr::instance()->saveChunk(arrId, attrId, outChunk->getId(), outChunk);
+
+	//----------------------------------------//
+	qry->getTimer()->pause(threadId);
+	//========================================//
+}
+
+size_t opIOAction::getSerializedChunkSize(attributeId attrId, pArray arr)
+{
+	size_t chunkSize = 0;
+	auto cit = arr->getChunkIterator(attrId, iterateMode::EXIST);
+	while (!cit->isEnd())
+	{
+		auto outChunk = (**cit);
+		chunkSize += outChunk->getSerializedSize();
+
+		++(*cit);
+	}
+
+	return chunkSize;
+}
+}		// core
+}		// msdb
